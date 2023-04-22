65fc9a3923a7c260dc7463523ac97fd67d53ee4d
Matthew Scarth <mscarth1@gmail.com>
2023-04-22 12:10:03 +0100

    update maths functions to accept more than two arguments

--- calc.js
+++ calc.js
@@ -1,17 +1,46 @@
 
+let num1;
+let num2;
+let operator;
 
-function add(a, b) {
-    return a + b;
+function operate(num1, operator, num2) {
+    if (operator == "+") {
+        add(num1, num2);
+    } else if (operator == "-") {
+        subtract(num1, num2); 
+    } else if (operator == "*") {
+        multiply(num2, num2);
+    } else if (operator == "/") {
+        divide(num1, num2)
+    }
 }
 
-function subtract(a, b) {
-    return a - b;
+function add(...arguments) {
+    let sum = 0;
+    for (i = 0; i < arguments.length; i++) {
+        sum += arguments[i];
+    }
+    return sum;
 }
 
-function multiply(a, b){
-    return a * b;
+function subtract(num1, ...arguments) {
+    for (i = 0; i < arguments.length; i++) {
+        num1 -= arguments[i];
+    }
+    return num1;
 }
 
-function divide(a, b) {
-    return a / b;
+function multiply(...arguments) {
+    let sum = 1;
+    for (i = 0; i < arguments.length; i++) {
+        sum *= arguments[i];
+    }
+    return sum;
+}
+
+function divide(num1, ...arguments) {
+    for (i = 0; i < arguments.length; i++) {
+        num1 /= arguments[i];
+    }
+    return num1;
 }
